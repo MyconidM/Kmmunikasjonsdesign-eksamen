bb36377eed3a8e4511891babf9b9e59e0648a889
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-11-10 02:07:00 +0100

    bevegende bie

---
+++ Infographic_story/script.js
@@ -0,0 +1,34 @@
+window.addEventListener("scroll", setScroll);
+window.addEventListener("resize", setScroll);
+let biePositionX = 0;
+
+function setScroll() {
+    const element = document.documentElement;
+    const moveBie = document.getElementsByClassName("bieConteiner");
+    const percentOfScreenHeightScroll = element.scrollTop / element.clientHeight;
+    const scrollCount = Math.min(percentOfScreenHeightScroll * 100);
+    console.log('Y: ' + scrollCount);
+    element.style.setProperty("--scroll", scrollCount);
+    console.log('X: ' + window.scrollX + document.querySelector('.bieConteiner').getBoundingClientRect().left);
+
+    if (scrollCount <= 110) {
+        // Scrolling down or initial movement
+        for (let i = 0; i < moveBie.length; i++) {
+            moveBie[i].style.transform = `translateX(calc(1% * (max(var(--scroll), 25) - 25) * 100/25))`;
+            biePositionX = window.scrollX + moveBie[i].getBoundingClientRect().left;
+        }
+    } else if (scrollCount <= 210 && scrollCount >= 110) {
+        // Scrolling up or reversing the movement
+        for (let i = 0; i < moveBie.length; i++) {
+            const newXPosition = biePositionX - (scrollCount - 110) * 2; // Adjust the factor as needed
+            moveBie[i].style.transform = `translateX(${newXPosition}px)`;
+        }
+    } else if (scrollCount <= 350 && scrollCount >= 230) {
+         for (let i = 0; i < moveBie.length; i++) {
+            const newXPosition = biePositionX + (scrollCount - 340) * 2; // Adjust the factor as needed
+            moveBie[i].style.transform = `translateX(${newXPosition}px)`;
+        }
+    }
+}
+
+setScroll();
